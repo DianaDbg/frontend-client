c8333a52f2faaf7e3b416dafa87cbd459ff29f53
Morlo, Mbakop <morlo.mbakop@relayr.io>
2021-07-24 13:33:39 +0200

    create language select options

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,17 @@
-import { HostListener, EventEmitter, Output } from '@angular/core';
-import { Component } from '@angular/core';
+import { Component, HostListener, EventEmitter, Output, OnInit } from '@angular/core';
+import { countries, Country, Language, languages } from 'countries-list';
+
+import { FlagsAndLanguage } from '../../shared/types';
 
 @Component({
   selector: 'app-navbar-1',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Output() toggleMenu = new EventEmitter();
   hasScrolled = false;
+  flagsAndLanguages: FlagsAndLanguage[] = [];
 
   @HostListener('window:scroll', ['$event'])
   onVerticalScroll(): void {
@@ -19,7 +22,27 @@ export class NavbarComponent {
     }
   }
 
+  ngOnInit(): void {
+    this.buildLanguagesOptions();
+  }
+
   onToggleMenu() {
     this.toggleMenu.emit();
+  }
+
+  onLanguageOptionChange(event: Event) {
+    // const select = event.target as HTMLSelectElement;
+    // console.log('HTMLSelectElement', select.value);
+  }
+
+  buildLanguagesOptions() {
+    void new Promise<void>(() => {
+      this.flagsAndLanguages = ['GB', 'DE', 'FR', 'US'].map((key) => {
+        const country = countries[key] as Country;
+        const lang = languages[country.languages[0]] as Language;
+        const value = country.languages[0];
+        return { displayName: `${country.emoji} ${lang.native}`, value };
+      });
+    });
   }
 }
---
+++ src/app/shared/types.ts
@@ -0,0 +1,1 @@
+export type FlagsAndLanguage = { displayName: string; value: string };
